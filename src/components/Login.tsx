00a1b8ca97bdb669642fb2034eacc4215707ba75
Jesus M Delgado D <m6nuel@gmail.com>
2024-10-30 20:11:45 -0300

    progresando en el login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -7,36 +7,49 @@ import UserMenu from "./UserMenu";
 const Login = () => {
   const { data: user } = useUser(); // Usuario autenticado actual de Firebase
   const auth = useAuth();
-  const { updateUser, user: userInContext } = useUserApp(); // Actualizar contexto del usuario
-
+  const { updateUser, user: userInContext, handleLogin, setInitialState, initialState } = useUserApp(); // Actualizar contexto del usuario
   // Sincronización inicial del estado del contexto con la sesión activa de Firebase
   useEffect(() => {
+    const storeToken = async () => {
+      if (user) {
+        try {
+          await handleLogin(user);
+          const token = await user.getIdToken();
+          localStorage.setItem("authToken", token);
+        } catch (error) {
+          console.error("no pudimos obtener el token", error);
+        }
+      }
+    };
     if (user && (!userInContext || user.uid !== userInContext.uid)) {
-      console.log("Sincronizando usuario al contexto...");
       updateUser(user); // Actualiza el contexto si hay un usuario activo y es diferente al del contexto
     }
-  }, [user, userInContext, updateUser]);
-  
+    storeToken();
+  }, [user, userInContext, updateUser, handleLogin]);
 
   // Inicio de sesión con Google
   const handleGoogleSignIn = async () => {
     const provider = new GoogleAuthProvider();
     const result = await signInWithPopup(auth, provider);
-    const user = result.user
+    const user = result.user;
     if (user) {
-      // console.log("Sesión iniciada con:", result.user);
-      updateUser(user); // Actualiza el contexto con el usuario recién autenticado
+      await handleLogin(user);
     }
-    // setTimeout(() => {
-    //   console.log("Sesión iniciada con:", result.user);
-    //   updateUser(result.user); // Actualiza el contexto con la información del usuario
-    // }, 0);
+    setInitialState({
+      ...initialState,
+      user: {
+        email: user.email || "", // Agregar valor predeterminado si está indefinido
+        displayName: user.displayName || "",
+        uid: user.uid,
+      },
+    });
   };
 
   // Cierre de sesión
   const handleLogout = async () => {
     await signOut(auth);
     updateUser(null); // Limpia el contexto al cerrar sesión
+    localStorage.removeItem("authToken");
   };
 
   // Renderizado basado en la autenticación del usuario
@@ -50,10 +63,7 @@ const Login = () => {
   }
 
   return (
-    <button
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      onClick={handleGoogleSignIn}
-    >
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleGoogleSignIn}>
       Login
     </button>
   );
--- src/hooks/useUserApp.tsx
+++ src/hooks/useUserApp.tsx
@@ -17,11 +17,34 @@ export const useUserApp = () => {
   const updateUser = (user: UserApp | null) => {
     // console.log("Actualizando usuario en el contexto a:", user);
     const us = user ? { email: user.email || null, displayName: user.displayName || null, uid: user.uid } : undefined
-    return setInitialState(({
+    setInitialState(({
       ...initialState,
       user: us, // Asigna el usuario o null si no está autenticado
     }));
+    return initialState;
   };
 
-  return { updateUser, user: initialState.user }; // Devolvemos también el usuario actual si lo necesitas
+  const handleLogin = async (user: UserApp) => {
+    const url = 'http://localhost:3000/api/v1/user'
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({email: user.email})
+      })
+
+      if (!response.ok) {
+        throw new Error('Credenciales incorrectas o error en el servidor');
+      }
+
+      const data = await response.json();
+      console.log(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  return { updateUser, user: initialState.user, handleLogin, setInitialState, initialState }; // Devolvemos también el usuario actual si lo necesitas
 };
